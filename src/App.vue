36cc8191f7f1094599cbbc61a95e16af0e36aab2
Devesh8817 <upadhyaydevesh971@gmail.com>
2025-06-10 15:14:38 +0530

    OK

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center h-screen " >
-    <div class="p-6 text-center  text-7xl text-black font-semibold bg-red-500 rounded-4xl ">
+    <div class="p-6 text-center  text-7xl text-black font-semibold bg-white-500 rounded-4xl ">
     Happy coding & good luck to you!
   </div>
   </div>
